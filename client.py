5d86a7b0969216a1ad0a8ba191d3cd017e1ac00d
Py-Code Tech <106017667+Py-CodeTech@users.noreply.github.com>
2023-08-17 17:17:34 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,14 +1,26 @@
+#Start-Process -WindowStyle hidden -FilePath Notepad.exe (Run in Powershell)
+
 import socket
+import time
 
 # Define the server's host and port
 HOST = '127.0.0.1'
 PORT = 12354
 
-# Create a socket object
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def establish_connection():
+    while True:
+        try:
+            # Create a socket object
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the server
-client_socket.connect((HOST, PORT))
+            # Connect to the server
+            client_socket.connect((HOST, PORT))
+            return client_socket
+        except (socket.error, ConnectionRefusedError):
+            print("Connection failed. Retrying in 5 seconds...")
+            time.sleep(5)
+
+client_socket = establish_connection()
 
 try:
     while True:
